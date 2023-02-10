19295202d74fa0c679af29651881fe6588c8bc2a
Fahmy Nd <travianerst12@gmail.com>
2023-02-10 11:23:51 +0800

    fixbug

--- js/scripts.js
+++ js/scripts.js
@@ -31,7 +31,7 @@ window.addEventListener('DOMContentLoaded', event => {
 
     // Activate Bootstrap scrollspy on the main nav element
     const mainNav = document.body.querySelector('#mainNav');
-    if (mainNav) {
+    if (!mainNav) {
         new bootstrap.ScrollSpy(document.body, {
             target: '#mainNav',
             offset: 72,
